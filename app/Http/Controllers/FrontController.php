e964fcab4890972020c1db84512e28ddd4ff59f7
hanissiddiq <hanissiddiq10@gmail.com>
2026-07-16 09:43:09 +0700

    code front article by category entertainment

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -36,7 +36,22 @@ class FrontController extends Controller
             // ->get();
             ->first();
 
-        return view('front.index', compact('categories', 'articles','authors', 'featured_articles','bannerads'));
+        $entertainment_articles = ArticleNews::whereHas('category', function ($query) {
+                $query->where('name', 'Entertainment');
+            })
+            ->where('is_featured', 'not_featured')
+            ->latest()
+            ->take(6)
+            ->get();
+
+        $entertainment_featured_articles = ArticleNews::whereHas('category', function ($query) {
+                $query->where('name', 'Entertainment');
+            })
+            ->where('is_featured', 'featured')
+            ->inRandomOrder()
+            ->first();
+
+        return view('front.index', compact('entertainment_articles', 'entertainment_featured_articles', 'categories', 'articles','authors', 'featured_articles','bannerads'));
     }
 
     public function details()
